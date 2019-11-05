28eb8fcd884e9f45f65f0b324d86906ed6aaf603
kota watanabe”git config --global user.name kota <watta.2co.3kyoku@gmail.com>
2019-11-05 10:12:10 +0900

    add Swift Project

---
+++ SoundSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundSample
+//
+//  Created by 渡邉康太 on 2019/11/05.
+//  Copyright © 2019 渡邉康太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoundSampleTests/SoundSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoundSampleTests.swift
+//  SoundSampleTests
+//
+//  Created by 渡邉康太 on 2019/11/05.
+//  Copyright © 2019 渡邉康太. All rights reserved.
+//
+
+import XCTest
+@testable import SoundSample
+
+class SoundSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
